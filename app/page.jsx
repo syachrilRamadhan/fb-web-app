ec08f0f13f5482442bbc96dea9f3e9c7e2e0f3b7
syachrilRamadhan <syachrilbls99@gmail.com>
2024-08-23 04:06:18 +0700

    feat: add footer page

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Footer from "@/components/Footer";
 
 const formatCurrency = (amount) => {
   return new Intl.NumberFormat("id-ID", {
@@ -12,7 +13,7 @@ const formatCurrency = (amount) => {
   }).format(amount);
 };
 
-const ITEMS_PER_PAGE = 10;
+const ITEMS_PER_PAGE = 9;
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -45,6 +46,7 @@ const Home = () => {
   const handlePageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
       setCurrentPage(newPage);
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
   };
 
@@ -79,15 +81,18 @@ const Home = () => {
         ))}
       </div>
 
-      <div className="mt-6 flex justify-center mx-2">
+      <div className="mt-6 mb-2 flex justify-center mx-2">
         <Button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="px-4 py-2 border bg-primary text-white border-white rounded-lg mr-2 disabled:opacity-50">
           Sebelumnya
         </Button>
         <span className="px-4 py-2 text-primary text-sm md:text-lg">
           Halaman {currentPage} dari {totalPages}
         </span>
-        <Button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className="px-4 py-2 border bg-primary text-white border-white rounded-lg mr-2 disabled:opacity-50">Selanjutnya</Button>
+        <Button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className="px-4 py-2 border bg-primary text-white border-white rounded-lg mr-2 disabled:opacity-50">
+          Selanjutnya
+        </Button>
       </div>
+      <Footer />
     </motion.div>
   );
 };
---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-gray-800 mt-12">
+      <footer className="text-white py-4">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2024 Family Battery. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
